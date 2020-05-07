c2d87442f936ed958cdf6143b04e3f4acc7b1088
vincentlieu <v.lieu93@gmail.com>
2020-05-07 14:40:28 +1000

    Created New room form

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,37 +1,36 @@
 class RoomsController < ApplicationController
-    def index
-        @rooms = Room.all
-    end
+  before_action :authenticate_user!
 
-    def show
-       
-    end
+  def index
+    @rooms = Room.all
+  end
 
-    def new
-        @room = Room.new
-    end
-    
-    def create
-        @room = Room.create(params.require(:room).permit(:title))
-    end
+  def show
+  end
 
-    def edit
-        
-    end
+  def new
+    @room = Room.new
+  end
 
-    def update
-        @room = Room.update(params["id"], params.require(:room).permit(:title))
-    end
+  def create
+    @room = Room.create(params.require(:room).permit(:title))
+  end
 
-    def destroy
-        Room.find(params[:id]).destroy
-    end
+  def edit
+  end
 
-    private
+  def update
+    @room = Room.update(params["id"], params.require(:room).permit(:title))
+  end
 
-    def set_room
-        id = params[:id]
-        @room = Room.find(id)
-    end
+  def destroy
+    Room.find(params[:id]).destroy
+  end
 
+  private
+
+  def set_room
+    id = params[:id]
+    @room = Room.find(id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get "/", to: "pages#home", as: "root"
+  get "/", to: "rooms#index", as: "root"
   devise_for :users
   #Room routes
   resources :rooms
+  resources :room_messages
 end
