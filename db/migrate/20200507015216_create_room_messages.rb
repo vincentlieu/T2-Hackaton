6d703be1c9b8de05337c152ad5afd009bdae1a77
wenzable <wendyuworks@gmail.com>
2020-05-07 12:14:53 +1000

    migration issue

--- db/migrate/20200507015216_create_room_messages.rb
+++ db/migrate/20200507015216_create_room_messages.rb
@@ -1,8 +1,8 @@
 class CreateRoomMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :room_messages do |t|
-      t.references :user_id, foreign_key: true
-      t.references :room_id, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :room, foreign_key: true
       t.text :description
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_003529) do
+ActiveRecord::Schema.define(version: 2020_05_07_015216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "room_messages", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "room_id"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_room_messages_on_room_id"
+    t.index ["user_id"], name: "index_room_messages_on_user_id"
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.bigint "user_id"
@@ -39,5 +49,7 @@ ActiveRecord::Schema.define(version: 2020_05_07_003529) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "room_messages", "rooms"
+  add_foreign_key "room_messages", "users"
   add_foreign_key "rooms", "users"
 end
